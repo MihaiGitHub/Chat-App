507d4ae273565abe328e881ed8fc1cee9667263f
Mihai Smarandache <mihai.sanfran@gmail.com>
2020-01-04 20:24:18 -0700

    Setup Express server

---
+++ src/index.js
@@ -0,0 +1,13 @@
+/* Load Node core module */
+const path = require('path')
+
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+app.use(express.static(publicDirectoryPath))
+
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}!`)
+})
